96356bc11ac99c4c2b9ed02a14428bad8efd200b
aashirwad43 <aashirwad43@gmail.com>
2021-10-06 12:12:04 -0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDzfMkoEZqbkidh5Pl-COwXpse5xBqO4z0",
+    authDomain: "clone-b4c66.firebaseapp.com",
+    projectId: "clone-b4c66",
+    storageBucket: "clone-b4c66.appspot.com",
+    messagingSenderId: "250130670996",
+    appId: "1:250130670996:web:612077d7ab4152bb7bdb81",
+    measurementId: "G-QDSMKWZM2J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var db = firebase.firestore();
